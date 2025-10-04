8abdb1a2fc8d9e9396c883398bc2833f2b24721f
Jurian <144108692+jurian404@users.noreply.github.com>
2025-10-04 21:42:53 +0200

    Added feedback option and more running options

--- main.js
+++ main.js
@@ -106,11 +106,16 @@ class Runner{
         this._values.push(text);
     }
 
-    _run(storage, input){
+    _run(storage, input, feedback = false){
+        const result = [];
         for(const task of this._values){
             if(typeof task === 'object'){
                 if(task instanceof Runner || task instanceof Loop){
-                    task.run(storage, input);
+                    const res = task.run(storage, input, feedback);
+                    if(res === false){
+                        return false;
+                    }
+                    result.push(...res);
                 }
             } else if (typeof task === 'string'){
                 switch (task){
@@ -127,33 +132,45 @@ class Runner{
                         storage.container().down();
                         break;
                     case ".":
-                        console.log("int: ", storage.container().get(), " --> ",  storage.container().read());
+                        const char = storage.container().read();
+                        const code = storage.container().get();
+                        feedback? console.log("out: ", code, " --> ",  char): null;
+                        result.push({char: char, code: code});
                         break;
                     case ",":
                         if(typeof input !== 'string'){
-                            console.error(`ERROR: The input must be a string. Got ${input}`);
-                            process.exit(1);
-                        }
-                        if(input.length === 0){
-                            console.error(`ERROR: The input must be defined to insert the input. Got "${input}"`);
-                            process.exit(1);
-                        }
-                        if(input.length !== 1){
-                            console.error(`ERROR: The input must be a single character. Got "${input}"`);
-                            process.exit(1);
+                            if(feedback){
+                                console.error(`ERROR: The input must be a string. Got ${typeof input}`);
+                            }
+                            return false;
+                        }else if(input.length === 0){
+                            if(feedback){
+                                console.error(`ERROR: The input must be defined to insert the input. Got "${input}"`);
+                            }
+                            return false;
+                        }else if(input.length !== 1){
+                            if(feedback){
+                                console.error(`ERROR: The input must be a single character. Got "${input}"`);
+                            }
+                            return false;
                         }
                         storage.container().set(input);
                         break;
                     default:
-                        console.warn("WARNING: Invalid value ", task, "\n Skipping this command.");
+                        feedback? console.warn("WARNING: Invalid value ", task, "\n Skipping this command."): null;
                         break;
                 }
             }
         }
+        return result;
     }
 
-    run(storage, input){
-        this._run(storage, input);
+    run(storage, input, feedback = false){
+        const result = this._run(storage, input, feedback);
+        if(result){
+            return result;
+        }
+        return false;
     }
 }
 
@@ -162,47 +179,87 @@ class Loop extends Runner{
         super(parent);
     }
 
-    run(storage, input){
+    run(storage, input, feedback = false){
+        const result = [];
         let counter = 0;
         while (storage.container().get() !== 0){
             counter += 1;
             if(counter > loopLimit){
-                console.log(`ERROR: The max amount of loops has reached: ${counter} \n Looks like a infinite loop`);
-                process.exit(1);
+                feedback? console.log(`ERROR: The max amount of loops has reached: ${counter} \n Looks like a infinite loop - stopping execution.`): null;
+                return false;
             }
-            this._run(storage, input);
+            const res = this._run(storage, input, feedback);
+            if(res === false){
+                return false;
+            }
+            result.push(...res);
         }
+        return result;
+    }
+}
+
+function builder(script){
+    const main = new Runner();
+    let currentElement = main;
+    for ( let i = 0; i < script.length; i++){
+        const currentCharacter = script[i];
+        if(currentCharacter === '['){
+            currentElement = currentElement.createChild();
+        } else if (currentCharacter === ']'){
+
+            currentElement = currentElement.closePart();
+        } else{
+            currentElement.addCharacter(currentCharacter);
+        }
+    }
+    return main;
+}
+
+function runWithFeedback(code, input){
+    try{
+        console.time('Finished build and ready to run after');
+        const built = builder(code);
+        console.timeEnd('Finished build and ready to run after');
+        console.time('Finished storage setup after');
+        const storage = new Storage();
+        console.timeEnd('Finished storage setup after');
+        console.time('Successfully finished script after');
+        const res = built.run(storage, input, true);
+        console.timeEnd('Successfully finished script after');
+        return res;
+    } catch (e){
+        console.error("ERROR: Something went wrong. ", e);
+        return false;
     }
 }
 
 function run(code, input){
-    function builder(script){
-        const main = new Runner();
-        let currentElement = main;
-        for ( let i = 0; i < script.length; i++){
-            const currentCharacter = script[i];
-            if(currentCharacter === '['){
-                currentElement = currentElement.createChild();
-            } else if (currentCharacter === ']'){
-
-                currentElement = currentElement.closePart();
-            } else{
-                currentElement.addCharacter(currentCharacter);
-            }
+    try{
+        const result = runDetailedInfo(code, input);
+        if(result === false){
+            return null;
         }
-        return main;
+        return result.map(e => e.char).join('');
+    } catch (e){
+        console.error("ERROR: Something went wrong. ", e);
     }
+}
 
-    console.time('Finished build and ready to run after');
-    const built = builder(code);
-    console.timeEnd('Finished build and ready to run after');
-    console.time('Finished storage setup after');
-    const storage = new Storage();
-    console.timeEnd('Finished storage setup after');
-    console.time('Successfully finished script after');
-    built.run(storage, input);
-    console.timeEnd('Successfully finished script after');
+function runDetailedInfo(code, input){
+    try{
+        const built = builder(code);
+        const storage = new Storage();
+        return built.run(storage, input);
+    } catch (e){
+        console.error("ERROR: Something went wrong. ", e);
+    }
 }
 
 
-module.exports = {run};
+module.exports = {run, runDetailedInfo, runWithFeedback};
+
+console.log(run(",.[.-]", "9"));
+console.log("next ->>>");
+console.log(runDetailedInfo(",.[.-]", "9"));
+console.log("next ->>>");
+console.log(runWithFeedback(",.[.-]", "9"));
